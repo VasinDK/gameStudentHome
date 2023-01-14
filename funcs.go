a8f827035f08370a19b48bffd3e64d1d38bb844d
Vasin.DK <vasindima@ya.ru>
2023-01-14 14:33:39 +0300

    start

---
+++ funcs.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func lookAround(par1 string, par2 string, par3 string) {
+	fmt.Println("lookAround")
+}
+
+func move(par1 string, par2 string, par3 string) {
+	fmt.Println("move")
+}
+
+func clothe(par1 string, par2 string, par3 string) {
+	fmt.Println("clothe")
+}
+
+func take(par1 string, par2 string, par3 string) {
+	fmt.Println("take")
+}
+
+func applay(par1 string, par2 string, par3 string) {
+	fmt.Println("applay")
+}
